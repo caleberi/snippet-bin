c17bfb6a4f9853e588c8dba239c7ff212f5e7572
caleberi <calerioluwa@gmail.com>
2023-02-16 17:37:54 +0100

    feat : database integration setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type config struct {
 	Addr      string
 	StaticDir string
+	dns       string
 }
 
 type application struct {
@@ -37,11 +42,20 @@ func main() {
 	cfg := new(config)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "http network address")
 	flag.StringVar(&cfg.StaticDir, "staticDir", "./ui/static/", "static directory")
+	flag.StringVar(&cfg.dns, "dns", "caleberi:test1234@/snippet_bin?parseTime=true", "database connection url")
 
 	flag.Parse()
 
 	infoLogger := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
 	errorLogger := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDatabase(cfg.dns)
+
+	if err != nil {
+		errorLogger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		config:      cfg,
@@ -50,4 +64,22 @@ func main() {
 	}
 
 	app.Serve()
+}
+
+func openDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	return db, nil
 }
